e7cbb318021ca907ee44e811bb57de6d1cd3b252
Boon <boonteck.soh@hotmail.com>
2021-07-22 22:28:58 +0800

    directed href to #

--- main.js
+++ main.js
@@ -11,7 +11,11 @@
 //     }
 // }
 
-function togglehidden() {
+function togglehidden(page) {
     let mainPage = document.getElementById("maincon")
     mainPage.classList.toggle("hidden")
+    console.log(page)
+    let articlePage = document.getElementById(page)
+    articlePage.classList.toggle("articlepagesflex")
 }
+
