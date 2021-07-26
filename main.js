991cfef7bdc0a9a328535e267c84c3a9fa8c317c
Boon <boonteck.soh@hotmail.com>
2021-07-26 15:26:37 +0800

    updated intro page with links

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-//
-// function toggleMainPage(){
-//     let mainPage = document.getElementById("maincon")
-//     console.log(mainPage.style)
-//     if (mainPage.style.display === "none"){
-//         mainPage.style.display = "none";
-//         console.log("test")
-//     } else {
-//         mainPage.style.display = "none"
-//         console.log("test")
-//     }
-// }
-
 function togglehidden(page) {
     let mainPage = document.getElementById("maincon")
     mainPage.classList.toggle("hidden")
